88da75ee7dd314efb9e7cddbea5f5daa79198f0f
AlbaDuqueC <alba.duque@iesnervion.es>
2025-12-17 09:46:28 +0100

    MongoDB

--- medico/db/client.py
+++ medico/db/client.py
@@ -1,3 +1,3 @@
 from pymongo import MongoClient
 
-db_client=MongoClient("mongodb+srv://albaduque_db_user:<db_password>@medico.5dz4jrc.mongodb.net/?appName=Medico")
+db_client=MongoClient("mongodb+srv://albaduque_db_user:<WjRQj0GvnQCNu8iD>@medico.5dz4jrc.mongodb.net/?appName=Medico")
